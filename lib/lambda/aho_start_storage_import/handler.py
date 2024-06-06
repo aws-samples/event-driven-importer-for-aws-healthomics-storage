e87ef25785d94c01c56e20a674be280f2478a1c0
Pang, Lee <pwyming@.amazon.com>
2024-06-06 16:45:58 -0700

    add/modify copyright headers

--- lib/lambda/aho_start_storage_import/handler.py
+++ lib/lambda/aho_start_storage_import/handler.py
@@ -1,5 +1,5 @@
 # /*********************************************************************************************************************
-# *  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+# *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
 # *                                                                                                                    *
 # *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
 # *  with the License. A copy of the License is located at                                                             *
@@ -10,6 +10,7 @@
 # *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
 # *  and limitations under the License.                                                                                *
 # *********************************************************************************************************************/
+
 import itertools as it
 import json
 import os
